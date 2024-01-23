ce9bceeda9615f520236153dc799632ff2573686
Molnar Mate <molnar.mate@evista.hu>
2024-01-23 22:24:38 +0100

    DatumEllen.java updates

--- iskolai_DatumEllen/src/DatumEllen.java
+++ iskolai_DatumEllen/src/DatumEllen.java
@@ -10,7 +10,7 @@ public class DatumEllen {
       // Év bekérése
       System.out.println("Kérek egy évszámot (1900 és 2024 között): ");
       ev = scanner.nextInt();
-      if (ev < 1900 || ev > 2024) { // negált művelete (ellentétse) a return miatt
+      if (ev < 1900 || ev > 2025) { // mint a hőmérsékletnél  4 - 2 - 0 - "-2" - "-4"
         System.out.println("Hibás évszám, a program véget ért!");
         return;
       }
@@ -18,7 +18,7 @@ public class DatumEllen {
       // Hónap bekérése
       System.out.println("Kérek egy hónapot (1 és 12 között): ");
       ho = scanner.nextInt();
-      if (ho < 1 || ho > 12) { // negált művelete (ellentétse) a return miatt
+      if (ho > 1 || ho < 12) { // negált művelete (ellentétse) a return miatt
         System.out.println("Hibás hónap, a program véget ért!");
         return;
       }
@@ -26,7 +26,7 @@ public class DatumEllen {
       // Nap bekérése
       System.out.println("Kérek egy napot: ");
       nap = scanner.nextInt();
-      if (nap < 1 || nap > 31) { // negált művelete (ellentétse) a return miatt
+      if (nap > 1 || nap < 31) { // negált művelete (ellentétse) a return miatt
         System.out.println("Hibás hónap, a program véget ért!");
         return;
       }
@@ -40,3 +40,25 @@ public class DatumEllen {
     }
   }
 }
+// package feladat;
+// import java.util.*;
+// public class DatumEllen {
+//     public static void main(String args[]) {
+//         Scanner a = new Scanner(System.in);
+//         int ev, ho, nap;
+//         try {
+//             System.out.println("Kérek egy évszámot: ");
+//             ev = a.nextInt();
+//             if (ev > 1900 || ev < 2024) {
+//             }
+//             System.out.println("Kérek egy hónapot: ");
+//             ho = a.nextInt();
+//             if (ho > 1 || ho < 12) {
+//             }
+//             System.out.println("Kérek egy napot: ");
+//             nap = a.nextInt();
+//             if (nap > 1 || nap < 31)
+//         }
+//         }
+//     }
+// }
